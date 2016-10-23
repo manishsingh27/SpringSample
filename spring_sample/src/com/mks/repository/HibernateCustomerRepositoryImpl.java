854caef99a4b3b2aa974728397335a4bb17dba79
MSingh <MSingh@LP-3411.prod1.lemansgroup.com>
2016-10-23 10:09:48 -0400

    Update to read the prop file

--- spring_sample/src/com/mks/repository/HibernateCustomerRepositoryImpl.java
+++ spring_sample/src/com/mks/repository/HibernateCustomerRepositoryImpl.java
@@ -3,6 +3,7 @@ package com.mks.repository;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 import com.mks.model.Customer;
@@ -14,12 +15,15 @@ public class HibernateCustomerRepositoryImpl implements HibernateCustomerReposit
 	 * @see com.mks.repository.HibernateCustomerRepository#findAll()
 	 */
 	
+	@Value("${sampleProp}")
+	private String samplePropValue;
+	
 	public List<Customer> findAll() {
 		List<Customer> customers = new ArrayList<>();
 
 		Customer customer = new Customer();
 
-		customer.setFirstName("testFirstName");
+		customer.setFirstName(samplePropValue);
 		customer.setLastName("testLastName");
 
 		customers.add(customer);
